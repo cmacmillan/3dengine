5f0e6a82f2ef3a15f345f561f7fe2754e3a3e275
Chase MacMillan <chasemacmillan@hotmail.com>
2025-01-04 20:20:13 -0800

    3d quad working again

--- engine/cpp/engine.cpp
+++ engine/cpp/engine.cpp
@@ -391,11 +391,14 @@ SShader::SShader(LPCWSTR lpcwstrFilename, bool fIs3D) : super()
 	}
 
 	// Create Input Layout
+
+	// BB force this to match with the input data
+
 	if (fIs3D)
 	{
 		D3D11_INPUT_ELEMENT_DESC inputElementDesc[] =
 		{
-			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 		};
 
